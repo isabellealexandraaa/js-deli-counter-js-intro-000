2039cb0f7ad791f29d1cc50641e49a5b3164b2f9
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-22 16:16:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function nowServing(katzDeliLine){
   return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
 }
+
+function currentLine(katzDeliLine) {
+  var string = `The line is currently:`;
+  var theNewLine = [];
+  if (katzDeliLine.length === 0)
+  return "The line is currently empty.";
+  else {
+    for (let i = 0; i < katzDeliLine.length; i++){
+      theNewLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
+    }
+    string += theNewLine.join(',')
+    return string;
+  }
+}
