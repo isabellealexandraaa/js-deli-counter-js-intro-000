4525247a7ea3314bb9b8da716eb73a81f1cc4268
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-22 16:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine) {
-  var string = `The line is currently:`;
+  var string = `The line is currently: `;
   var theNewLine = [];
   if (katzDeliLine.length === 0)
   return "The line is currently empty.";
