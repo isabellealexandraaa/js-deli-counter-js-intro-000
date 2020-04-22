b3b277045840298d27c8be6b917071894746c66c
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-22 16:04:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-var katzDeliLine = [];
-
-function takeANumber (katzDeliLine, newName) {
-  console.log (`Welcome, ${newName}. You are number in line`)
-
-  //indexOf(katzDeliLine) = i + 1
-}
-
-function nowServing (katzDeliLine) {
-  console.log(katzDeliLine.shift())
-if (katzDeliLine.length <= 0){
-  return "There is nobody waiting to be served."
-}
-}
-
-function currentLine (katzDeliLine) {
-  if (katzDeliLine.length <= 0){
-    return "The line is currently empty."
+function takeANumber (katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome ${name}. You are number ${katzDeliLine.length} in line`;
 }
